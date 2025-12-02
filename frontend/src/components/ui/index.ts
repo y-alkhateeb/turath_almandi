891ef8678cb2417dbfd2a1a97d09e61e893145be
y-alkhateeb <eng.y.alkhateeb@gmail.com>
2025-12-03 00:11:32 +0100

    fix deploy error

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -62,4 +62,4 @@ export {
   DialogDescription,
 } from './dialog';
 export { Badge, badgeVariants, type BadgeProps } from './badge';
-export { Skeleton } from './skeleton';
+export { Skeleton } from './Skeleton';
